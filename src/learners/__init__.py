7e1cc74cea7db297d5bb2e0457ff065b7d5ab5f8
ChentaiQiao <62085175+chentaiqiao@users.noreply.github.com>
2024-03-13 22:17:56 +0800

    Update __init__.py

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,10 +1,9 @@
 from .q_learner import QLearner
-from .coma_learner import COMALearner
-from .categorical_q_learner import CateQLearner
+from .ccdg_q_learner import CCDGQLearner
+
 
 
 REGISTRY = {
     "q_learner": QLearner,
-    "coma_learner": COMALearner,
-    "cate_q_learner": CateQLearner
+    "ccdg_q_learner": QLearner
 }
